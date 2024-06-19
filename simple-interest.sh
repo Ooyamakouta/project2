24c07dc40d7e1e862acf8d98c2b93dcc7cf082da
Ilyas Tleukhan <iliastk1504@gmail.com>
2024-06-19 15:58:09 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+# Скрипт для вычисления простого процента
+# Использование: ./simple-interest.sh principal rate time
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 principal rate time"
+  exit 1
+fi
+
+principal=$1
+rate=$2
+time=$3
+
+interest=$(echo "$principal * $rate * $time / 100" | bc -l)
+
+echo "The simple interest is: $interest"
 
